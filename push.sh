f8c6f6104e5aaf7d7b6a69106ea18bdc0e258f20
fwdzh <wu_yi_@outlook.com>
2025-04-29 01:16:56 +0800

    update on 2025-04-29 01:16:56

---
+++ push.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sss="update on $(date '+%Y-%m-%d %H:%M:%S')"
+if [ -z "$1" ]; then
+  msg="update on $(date '+%Y-%m-%d %H:%M:%S')"
+else
+  msg=""$1" $(date '+%Y-%m-%d %H:%M:%S')"
+fi
+
+# echo "$msg"
+git add .
+git commit -m "$msg"
+git push
+
+cd ../cp_code
+git add .
+git commit -m "$sss"
+git push
