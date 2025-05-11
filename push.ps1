5e1d2d8ae339d1c2523fa90ed2745a784c2f5158
fwdzh <wu_yi_@outlook.com>
2025-05-11 14:20:58 +0800

    包子杯2 2025-05-11 14:20:58

--- push.ps1
+++ push.ps1
@@ -10,12 +10,3 @@ git add .
 git commit -m "$msg"
 git push
 git push gitee main
-
-Set-Location ..\cp_code
-$sss = "update on $time"
-git add .
-git commit -m "$msg"
-# git commit -m "$sss"
-git push
-git push gitee master
-Set-Location ..\blog
